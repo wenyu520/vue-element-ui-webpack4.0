96cf651a9b552c00e73dcdd17eb192cf65474ce1
seven <sevenjustfine@qq.com>
2019-11-05 16:56:33 +0800

    '..'

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,10 @@
 <script>
   export default {
     name: 'app',
-    data() {
-      return {
-        message: 'BASIC模块的内容'
-      }
-    }
   }
 </script>
 <style>
-    @import './assets/css/base.css';
-    @import './assets/css/common.css';
-    @import "./assets/font-awesome-4.7.0/css/font-awesome.min.css";
-    /*@import '//at.alicdn.com/t/font_561828_tymp33g1b1.css';*/
-    @import "./assets/iconFont/iconfont.css";
-    @import "./assets/css/selectTable.css";
-    @import "./assets/css/tooltip.css";
+
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
---
+++ src/components/page404.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "page404"
+    }
+</script>
+
+<style scoped>
+
+</style>
